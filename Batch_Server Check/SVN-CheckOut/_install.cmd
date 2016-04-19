258958faccac6bac6ee6c6ad37389bc947a6e6d7
charles-miao <fengkuang33@163.com>
2016-04-19 22:52:33 +0800

    delete un-autoit program

--- Batch_Server Check/SVN-CheckOut/_install.cmd
+++
@@ -1,2 +0,0 @@
-for %%I in (%cd%\*.bat) do set cmd=%%~nxI
-schtasks /create /tn "%cmd%" /sc minute /mo 1 /tr "%cd%\%cmd%"
--- Batch_Server Check/SVN-CheckOut/commit/post-commit.cmd
+++
@@ -1,4 +0,0 @@
-set REPOS=%1
-set REV=%2
-C:\SubversionNotify\SubversionNotify.exe %REPOS% %REV% -post
-
